13c750930af439a65e60f9847add41209dd969a7
Ananta Pratama <deras.hujan15@gmail.com>
2018-07-01 19:48:37 +0700

    Initial Commit

---
+++ Warung Daging/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Warung Daging
+//
+//  Created by Ananta Pratama on /1Jul/18.
+//  Copyright © 2018 Ignitech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
