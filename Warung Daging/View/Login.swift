974a1af11d221c10ffdcf96cfe651ce9ff09a676
Ananta Pratama <deras.hujan15@gmail.com>
2018-07-02 20:07:32 +0700

    Added auth screens

---
+++ Warung Daging/View/Login.swift
@@ -0,0 +1,53 @@
+//
+//  Login.swift
+//  Warung Daging
+//
+//  Created by Ananta Pratama on /2Jul/18.
+//  Copyright © 2018 Ignitech. All rights reserved.
+//
+
+import UIKit
+
+class Login: UIViewController {
+
+    @IBAction func backAction() {
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        
+        // Hide the navigation bar
+        self.navigationController?.setNavigationBarHidden(true, animated: true)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Make navigation bar visible again
+        self.navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
